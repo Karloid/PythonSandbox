a7f6246a4ed57217585b3c67faa0b5f125d1f895
home <home@works-iMac.local>
2016-02-21 02:16:03 +0300

    Initial commit

---
+++ HelloWorld.py
@@ -0,0 +1,12 @@
+print "Hello world " + str(3 + 3)
+
+# name1 = raw_input("your name:")
+# print "your name is " + name1
+
+if 1 > 2:
+    print  '1 > 2'
+else:
+    print '2 > 1'
+
+
+print "End of program lol"
